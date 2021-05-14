01a8a77b0ee06905ebdfb15d640afb1ae49273ea
alfred <programmingwithalfred@gmail.com>
2021-05-14 18:40:17 +0000

    Updates

---
+++ precache-manifest.af057bc02a379ac7e7eb5e9cc11e9861.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f06feb43dd1d00988056",
+    "url": "/stack_overflow_clone/static/css/main.3f29ef31.chunk.css"
+  },
+  {
+    "revision": "f06feb43dd1d00988056",
+    "url": "/stack_overflow_clone/static/js/main.de848f55.chunk.js"
+  },
+  {
+    "revision": "d3fa51358d04ec0b240f",
+    "url": "/stack_overflow_clone/static/js/runtime~main.ef826049.js"
+  },
+  {
+    "revision": "2e8b1acfdac75932d4d9",
+    "url": "/stack_overflow_clone/static/css/2.e2224ade.chunk.css"
+  },
+  {
+    "revision": "2e8b1acfdac75932d4d9",
+    "url": "/stack_overflow_clone/static/js/2.ad59097b.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/stack_overflow_clone/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/stack_overflow_clone/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/stack_overflow_clone/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/stack_overflow_clone/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/stack_overflow_clone/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/stack_overflow_clone/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/stack_overflow_clone/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/stack_overflow_clone/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/stack_overflow_clone/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/stack_overflow_clone/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/stack_overflow_clone/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/stack_overflow_clone/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/stack_overflow_clone/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/stack_overflow_clone/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/stack_overflow_clone/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/stack_overflow_clone/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "de43804e11794db2e88f78cf13f3d29d",
+    "url": "/stack_overflow_clone/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/stack_overflow_clone/precache-manifest.af057bc02a379ac7e7eb5e9cc11e9861.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/stack_overflow_clone/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
